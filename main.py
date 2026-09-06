9f9e8a6653be398ae3937d2aec03b699004075d7
beurrekk <suphannika.kk@gmail.com>
2026-09-07 04:06:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,39 +11,20 @@ st.text("by Suphannika")
 
 
 
-@st.cache_data(ttl="6h")
-def load_data():
+allproduct_conn = st.connection("allproduct_gsheet", type=GSheetsConnection)
+allproduct_df = allproduct_conn.read(ttl=21600)
 
-    allproduct_conn = st.connection(
-        "allproduct_gsheet",
-        type=GSheetsConnection
-    )
-    allproduct_df = allproduct_conn.read()
+affiliate_conn = st.connection("affiliate_gsheet", type=GSheetsConnection)
+affiliate_df = affiliate_conn.read(ttl=21600)
 
-    affiliate_conn = st.connection(
-        "affiliate_gsheet",
-        type=GSheetsConnection
-    )
-    affiliate_df = affiliate_conn.read()
+mcn_conn = st.connection("mcn_gsheet", type=GSheetsConnection)
+mcn_df = mcn_conn.read(ttl=21600)
 
-    mcn_conn = st.connection(
-        "mcn_gsheet",
-        type=GSheetsConnection
-    )
-    mcn_df = mcn_conn.read()
-
-    kalodata_conn = st.connection(
-        "kalodata_gsheet",
-        type=GSheetsConnection
-    )
-    kalodata_df = kalodata_conn.read()
-
-    return allproduct_df, affiliate_df, mcn_df, kalodata_df
+kalodata_conn = st.connection("kalodata_gsheet", type=GSheetsConnection)
+kalodata_df = kalodata_conn.read(ttl=21600)
 
 
-allproduct_df, affiliate_df, mcn_df, kalodata_df = load_data()
-
-st.write(allproduct_df.shape)
-st.write(affiliate_df.shape)
-st.write(mcn_df.shape)
-st.write(kalodata_df.shape)
+st.write("All Product:", allproduct_df.shape)
+st.write("Affiliate:", affiliate_df.shape)
+st.write("MCN:", mcn_df.shape)
+st.write("Kalodata:", kalodata_df.shape)
