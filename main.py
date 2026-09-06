33bdd94edf174eb4adb9a6eb79b93e71c9808271
beurrekk <suphannika.kk@gmail.com>
2026-09-07 04:13:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,39 @@ st.title(":blue[MizuMi] TIKTOK PERFORMANCE")
 st.text("by Suphannika")
 
 
+@st.cache_data(ttl="6h")
+def load_data():
+
+    allproduct_conn = st.connection(
+        "allproduct_gsheet",
+        type=GSheetsConnection
+    )
+    allproduct_df = allproduct_conn.read()
+
+    affiliate_conn = st.connection(
+        "affiliate_gsheet",
+        type=GSheetsConnection
+    )
+    affiliate_df = affiliate_conn.read()
+
+    mcn_conn = st.connection(
+        "mcn_gsheet",
+        type=GSheetsConnection
+    )
+    mcn_df = mcn_conn.read()
+
+    kalodata_conn = st.connection(
+        "kalodata_gsheet",
+        type=GSheetsConnection
+    )
+    kalodata_df = kalodata_conn.read()
+
+    return allproduct_df, affiliate_df, mcn_df, kalodata_df
+
+
+allproduct_df, affiliate_df, mcn_df, kalodata_df = load_data()
+
+st.write(allproduct_df.shape)
+st.write(affiliate_df.shape)
+st.write(mcn_df.shape)
+st.write(kalodata_df.shape)
